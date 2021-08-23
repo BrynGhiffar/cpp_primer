37b5b4d224b70238b95b48a37636db56da163649
Bryn <bryn.ghiffar@gmail.com>
2021-08-24 06:37:09 +0700

    Chapter 11 Listing 4 to Listing 6

---
+++ chapter11/l4tol6/mytime1.h
@@ -0,0 +1,20 @@
+// 11.4 mytime1.h -- Time class before operator overloading
+#ifndef MYTIME1_H_
+#define MYTIME1_H_
+
+class Time
+{
+	private:
+		int m_hours;
+		int m_minutes;
+	public:
+		Time();
+		Time(int h, int m = 0);
+		void AddMin(int m);
+		void AddHr(int h);
+		void Reset(int h = 0, int m = 0);
+		Time operator+(const Time & t) const;
+		void Show() const;
+};
+
+#endif
