2969da4c651c0c62d9d68d5534f40c152919aa64
Bryn <bryn.ghiffar@gmail.com>
2021-08-23 14:32:25 +0700

    Chapter 11 Listing 1 to Listing 3

---
+++ chapter11/l1tol3/mytime0.cpp
@@ -0,0 +1,46 @@
+// 11.2 mytime0.cpp -- implementing Time methods
+#include <iostream>
+#include "mytime0.h"
+
+Time::Time()
+{
+	m_hours = m_minutes = 0;
+}
+
+Time::Time(int h, int m)
+{
+	m_hours = h;
+	m_minutes = m;
+}
+
+void Time::AddMin(int m)
+{
+	m_minutes += m;
+	m_hours += m_minutes / 60;
+	m_minutes %= 60;
+}
+
+void Time::AddHr(int h)
+{
+	m_hours += h;
+}
+
+void Time::Reset(int h, int m)
+{
+	m_hours = h;
+	m_minutes = m;
+}
+
+Time Time::Sum(const Time & t) const
+{
+	Time sum;
+	sum.m_minutes = m_minutes + t.m_minutes;
+	sum.m_hours = m_hours + t.m_hours + sum.m_minutes / 60;
+	sum.m_minutes %= 60;
+	return sum;
+}
+
+void Time::Show() const
+{
+	std::cout << m_hours << " hours, " << m_minutes << " minutes";
+}
---
+++ chapter11/l1tol3/mytime0.h
@@ -0,0 +1,20 @@
+// 11.1 mytime0.h -- Time class before operator overloading
+#ifndef MYTIME0_H_
+#define MYTIME0_H_
+
+class Time
+{
+	private:
+		int m_hours;
+		int m_minutes;
+	public:
+		Time();
+		Time(int h, int m = 0);
+		void AddMin(int m);
+		void AddHr(int h);
+		void Reset(int h = 0, int m = 0);
+		Time Sum(const Time & t) const;
+		void Show() const;
+};
+
+#endif
