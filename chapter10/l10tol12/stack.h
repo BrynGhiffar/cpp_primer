2d8c96ac83ca1115c0f347e776d4a31802348667
Bryn <bryn.ghiffar@gmail.com>
2021-08-23 10:20:41 +0700

    listing 10 to listing 12 notes

---
+++ chapter10/l10tol12/stack.h
@@ -0,0 +1,22 @@
+// 10.10 stack.h -- class definition for the stack ADT
+#ifndef STACK_H_
+#define STACK_H_
+
+typedef unsigned long Item;
+
+class Stack 
+{
+	private:
+		enum {MAX = 10};
+		Item items[MAX];
+		int top;
+	public:
+		Stack();
+		bool isempty() const;
+		bool isfull() const;
+		// push() returns false if stack is already full, true otherwise
+		bool push(const Item & item); 		// add item to the stack
+		// pop() returns false if stack is already empty, true otherwise
+		bool pop(Item & item); 		// pop top into item
+};
+#endif
